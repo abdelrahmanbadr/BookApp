c31543dd21d244b51f719530c9dbe2a483d1e699
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2019-03-20 13:21:31 +0200

    add blank controller & book service factory

---
+++ app/Domain/Services/Factory/BookServiceFactory.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abdelrahmanbadr
+ * Date: 3/20/19
+ * Time: 1:19 PM
+ */
+
+namespace App\Domain\Services\Factory;
+
+use App\Domain\Contracts\BookServiceInterface;
+use App\Domain\Entities\Book;
+use App\Domain\Mappers\BookCollectionMapper;
+use App\Domain\Repositories\EloquentBookRepository;
+use App\Domain\Services\{Criteria\CriteriaBuilder, BookService};
+
+
+class BookServiceFactory
+{
+
+    /**
+     * @return BookServiceInterface
+     */
+    public function make(): BookServiceInterface
+    {
+        $bookMapper = new BookCollectionMapper();
+        $bookRepository = new EloquentBookRepository(new Book());
+        $bookRepository->setCriteria(new CriteriaBuilder());
+        return new BookService($bookRepository, $bookMapper);
+    }
+}

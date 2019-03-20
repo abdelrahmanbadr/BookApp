5abcc9a7b930130b6032d73e38a2fe3682dded0a
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2019-03-20 12:41:02 +0200

    adding string helper

---
+++ app/Domain/Mappers/BookCollectionMapper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abdelrahmanbadr
+ * Date: 3/20/19
+ * Time: 12:14 PM
+ */
+
+namespace App\Domain\Mappers;
+
+
+class BookCollectionMapper
+{
+
+}
---
+++ app/Helpers/StringHelper.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abdelrahmanbadr
+ * Date: 3/20/19
+ * Time: 12:35 PM
+ */
+
+namespace App\Helpers;
+
+
+class StringHelper
+{
+    /**
+     *  Uppercase the first character of each word in a string
+     * @param string $attribute
+     * @return string
+     */
+    public static function upperCaseFirstChars(string $attribute)
+    {
+        return ucwords(strtolower($attribute));
+    }
+
+}

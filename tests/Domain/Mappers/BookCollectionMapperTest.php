1761523a39f7174efa95b2aebbea3418d41cd746
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2019-03-20 13:00:53 +0200

    test book collection mapper

---
+++ tests/Domain/Mappers/BookCollectionMapperTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abdelrahmanbadr
+ * Date: 3/20/19
+ * Time: 12:56 PM
+ */
+
+namespace Tests\Domain\Mappers;
+
+
+use App\Domain\Mappers\BookCollectionMapper;
+use Illuminate\Support\Collection;
+use Tests\TestCase;
+
+class BookCollectionMapperTest extends TestCase
+{
+    public function dataProvider()
+    {
+        return [
+            [
+                "books" => collect([
+                    (object)[
+                        "id" => 1,
+                        "title" => "Refactoring: Improving the Design of Existing Code",
+                        "authorName" => "martin fowler",
+                    ],
+                    (object)[
+                        "id" => 2,
+                        "title" => "Testing Computer Software",
+                        "authorName" => "cem kaner",
+                    ]
+                ]),
+                "expectedMappedBooks" => [
+                    (object)[
+                        "id" => 1,
+                        "title" => "Refactoring: Improving The Design Of Existing Code",
+                        "authorName" => "Martin Fowler"
+                    ],
+                    (object)[
+                        "id" => 2,
+                        "title" => "Testing Computer Software",
+                        "authorName" => "Cem Kaner",
+                    ],
+
+
+                ],
+
+            ],
+        ];
+
+    }
+
+
+    /**
+     * testing map  book collection
+     *
+     * @param Collection $books
+     * @param array $expectedMappedBooks
+     *
+     * @dataProvider dataProvider
+     * @return void
+     */
+    public function testBookMapper(Collection $books, array $expectedMappedBooks)
+    {
+        $mapper = new BookCollectionMapper();
+
+        $actualMappedBook = $mapper->map($books);
+        $this->assertEquals($actualMappedBook, $expectedMappedBooks);
+    }
+
+
+}

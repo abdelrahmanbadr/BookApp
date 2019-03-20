e10c6cbcbf234fc55fa0b39321d9be64163b8ee5
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2019-03-20 12:13:12 +0200

    working on book service

--- app/Domain/Contracts/BookServiceInterface.php
+++ app/Domain/Contracts/BookServiceInterface.php
@@ -12,21 +12,10 @@ namespace App\Domain\Contracts;
 interface BookServiceInterface extends CrudInterface
 {
     /**
-     * @return mixed
-     */
-    public function getAllWithAuthorName();
-
-    /**
-     * @param array $attributes
-     * @return mixed
-     */
-    public function createBookAndAuthorIfNotExist(array $attributes);
-
-    /**
      * @param string $exportType
      * @param array $attributes
      * @return string
      */
     public function export(string $exportType, array $attributes): string;
-    
+
 }
---
+++ app/Domain/Services/BookService.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abdelrahmanbadr
+ * Date: 3/20/19
+ * Time: 12:06 PM
+ */
+
+namespace App\Domain\Services;
+
+use App\Domain\Contracts\{BookRepositoryInterface, BookServiceInterface, CollectionMapperInterface};
+
+
+class BookService implements BookServiceInterface
+{
+
+    /**
+     * @var CollectionMapperInterface
+     */
+    private $mapper;
+    /**
+     * @var BookRepositoryInterface
+     */
+    private $repository;
+
+
+    /**
+     * BookService constructor.
+     * @param BookRepositoryInterface $repository
+     * @param CollectionMapperInterface $mapper
+     */
+    public function __construct(BookRepositoryInterface $repository, CollectionMapperInterface $mapper)
+    {
+        $this->repository = $repository;
+        $this->mapper = $mapper;
+
+    }
+
+    /**
+     * @param array $attributes
+     * @return mixed
+     */
+    public function getAll($attributes = ['*'])
+    {
+        $books = $this->repository->getAll($attributes);
+        return $this->mapper->map($books);
+    }
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function get(int $id)
+    {
+        return $this->repository->get($id);
+
+    }
+
+    /**
+     * @param array $attributes
+     * @return mixed
+     */
+    public function create(array $attributes)
+    {
+        return $this->repository->create($attributes);
+    }
+
+    /**
+     * @param int $id
+     * @param array $attributes
+     * @return mixed
+     */
+    public function update(int $id, array $attributes)
+    {
+
+        return $this->repository->update($id, $attributes);
+    }
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function delete(int $id)
+    {
+        return $this->repository->delete($id);
+    }
+
+    /**
+     * @param string $exportType
+     * @param array $attributes
+     * @return string
+     */
+    public function export(string $exportType, array $attributes): string
+    {
+        //@todo implement here
+    }
+
+    /**
+     * @param CollectionMapperInterface $mapper
+     */
+    public function setMapper(CollectionMapperInterface $mapper)
+    {
+        $this->mapper = $mapper;
+    }
+
+
+}
